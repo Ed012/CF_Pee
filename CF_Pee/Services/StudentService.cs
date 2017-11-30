4eef96cb7fb92ae466d18be20ebd38644cbd080b
Wilbor <wilborma2009@hotmail.com>
2017-11-30 09:00:38 -0500

    Prueba Unitaria IsValid_StudentName

---
+++ CF_Pee/Services/StudentService.cs
@@ -0,0 +1,25 @@
+﻿using CF_Pee.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CF_Pee.Services
+{
+    public class StudentService
+    {
+        public List<string> Errores { get; private set; }
+        public bool IsValid(Student student)
+        {
+            Errores = new List<string>();
+
+            var valid = true;
+            if (string.IsNullOrWhiteSpace(student.Name))
+            {
+                Errores.Add("Se requiere el nombre del estudiante");
+                valid = false;
+            }
+            return valid;
+        }
+    }
+}
---
+++ UnitTestProject1/StudentServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CF_Pee.Services;
+using CF_Pee.Models;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class StudentServiceTest
+    {
+        [TestMethod]
+        public void IsValid_StudentNameEMpty()
+        {
+            var service = new StudentService();
+            var student = new Student() { Name = string.Empty };
+
+            var result = service.IsValid(student);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
