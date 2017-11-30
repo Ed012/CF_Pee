311023a91c0859bd9ed209caa31be0d6a8d1d08a
Wilbor <wilborma2009@hotmail.com>
2017-11-30 13:10:44 -0500

    Agregando prueba unitaria nota1

---
+++ CF_Pee/Services/NoteService.cs
@@ -0,0 +1,25 @@
+﻿using CF_Pee.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CF_Pee.Services
+{
+    public class NoteService
+    {
+        public List<string> Errores { get; private set; }
+        public bool IsValid(Notes note)
+        {
+            Errores = new List<string>();
+
+            var valid = true;
+            if (note.First_Test < 0)
+            {
+                Errores.Add("Se requiere una nota mayor a 0 ");
+                valid = false;
+            }
+            return valid;
+        }
+    }
+}
---
+++ UnitTestProject1/NoteServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CF_Pee.Services;
+using CF_Pee.Models;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class NoteServiceTest
+    {
+        [TestMethod]
+        public void First_Test_Note()
+        {
+            //Arrange 
+            var service = new NoteService();
+            var note = new Notes() { First_Test = -3 };
+
+            //Act
+            var result = service.IsValid(note);
+
+            //Assert	
+            Assert.IsFalse(result);
+        }
+    }
+}
