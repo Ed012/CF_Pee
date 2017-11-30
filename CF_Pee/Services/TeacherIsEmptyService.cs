d68b9b157dcf91f3fec154844c90020ed22fcf2e
Wilbor <wilborma2009@hotmail.com>
2017-11-30 12:01:52 -0500

    Prueba Unitaria TeacherIsEmptyName, Agregando CourseAPIController

---
+++ CF_Pee/Services/TeacherIsEmptyService.cs
@@ -0,0 +1,25 @@
+﻿using CF_Pee.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CF_Pee.Services
+{
+    public class TeacherIsEmptyService
+    {
+        public List<string> Errores { get; private set; }
+        public bool IsValid(Teacher teacher)
+        {
+            Errores = new List<string>();
+
+            var valid = true;
+            if (string.IsNullOrWhiteSpace(teacher.Name))
+            {
+                Errores.Add("Se requiere el nombre del docente");
+                valid = false;
+            }
+            return valid;
+        }
+    }
+}
--- UnitTestProject1/TeacherServiceTest.cs
+++ UnitTestProject1/TeacherServiceTest.cs
@@ -1,22 +1,22 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using CF_Pee.Services;
 using CF_Pee.Models;
+using CF_Pee.Services;
 
 namespace UnitTestProject1
 {
     [TestClass]
-    public class StudentServiceTest
+    public class TeacherServiceTest
     {
         [TestMethod]
-        public void IsValid_StudentNameEMpty()
+        public void IsEmpty_TeacherName()
         {
             //Arrange 
-            var service = new StudentEmptyService();
-            var student = new Student() { Name = string.Empty };
+            var service = new TeacherIsEmptyService();
+            var teacher = new Teacher() { Name = string.Empty };
 
             //Act
-            var result = service.IsValid(student);
+            var result = service.IsValid(teacher);
 
             //Assert	
             Assert.IsFalse(result);
